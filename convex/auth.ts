a6d4d5d848c531e24fd0547ddde08d11e033db21
Han <hander.juan@gmail.com>
2025-09-12 19:05:53 +0200

    refactor: improve Discord authentication setup and reorganize imports

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,11 +1,14 @@
+import Discord from "@auth/core/providers/discord";
 import { Password } from "@convex-dev/auth/providers/Password";
-import Discord from "@auth/core/providers/discord"
 
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password, Discord({
-    clientId: process.env.DISCORD_AUTH_CLIENT_ID!,
-    clientSecret: process.env.DISCORD_AUTH_CLIENT_SECRET!,
-  })],
+  providers: [
+    Password,
+    Discord({
+      clientId: process.env.DISCORD_AUTH_CLIENT_ID ?? "",
+      clientSecret: process.env.DISCORD_AUTH_CLIENT_SECRET ?? "",
+    }),
+  ],
 });
