5239d8019e7320fd0a5ecad51bfa2eb1853403f1
luisjedev <luisjedev@gmail.com>
2025-09-12 18:47:15 +0200

    clean up

--- app/design-system/page.tsx
+++ app/design-system/page.tsx
@@ -1,14 +1,11 @@
 export default function DesignSystem() {
   return (
     <>
-      <header className="sticky top-0 z-10 bg-neutral-50 p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
+      <header className="sticky top-0 z-10 flex flex-row items-center justify-between border-slate-200 border-b-2 bg-neutral-50 p-4 dark:border-slate-800">
         Design System
       </header>
-      <main className="p-8 flex flex-col gap-8">
-        <h1 className="text-4xl font-bold text-center">
-          Example components
-        </h1>
-
+      <main className="flex flex-col gap-8 p-8">
+        <h1 className="text-center font-bold text-4xl">Example components</h1>
       </main>
     </>
   );
