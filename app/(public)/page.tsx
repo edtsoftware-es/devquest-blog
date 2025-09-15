9df9572ddcd1139a82515f5c13f0d59f7dab7c36
luisjedev <luisjedev@gmail.com>
2025-09-15 20:48:26 +0200

    fix: remove ModeToggle component from Home page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,5 +1,4 @@
 import { fetchQuery } from "convex/nextjs";
-import { ModeToggle } from "@/components/mode-toggle";
 import { SignOutButton } from "@/components/sign-out-button";
 import { api } from "@/convex/_generated/api";
 
@@ -7,7 +6,6 @@ export default async function Home() {
   const posts = await fetchQuery(api.posts.getPublishedPosts, {});
   return (
     <main>
-      <ModeToggle />
       <h1 className="font-bold text-3xl underline">Hello, DevQuest!</h1>
       {posts.page.map((post) => {
         return <p key={post._id}>{JSON.stringify(post)}</p>;
