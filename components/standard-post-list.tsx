f555830e22bc9356c2f6e83096981cabda689016
luisjedev <luisjedev@gmail.com>
2025-09-20 20:08:16 +0200

    feat: add HomePagination component for improved post navigation

--- components/standard-post-list.tsx
+++ components/standard-post-list.tsx
@@ -5,6 +5,8 @@ import {
   usePaginatedQuery,
   usePreloadedQuery,
 } from "convex/react";
+import { useState } from "react";
+import HomePagination from "@/app/(public)/components/home-pagination";
 import { DEFAULT_LATEST_POSTS_LIMIT } from "@/app/(public)/page";
 import { api } from "@/convex/_generated/api";
 import { cn } from "@/lib/utils";
@@ -18,7 +20,9 @@ export default function StandardPostList({
   totalPosts: number;
   className?: string;
 }) {
+  const [currentPage, setCurrentPage] = useState(1);
   const firstPage = usePreloadedQuery(preloaded);
+  const totalPages = Math.ceil(totalPosts / DEFAULT_LATEST_POSTS_LIMIT);
 
   const { results, status, loadMore } = usePaginatedQuery(
     api.posts.getLatestPosts,
@@ -27,8 +31,6 @@ export default function StandardPostList({
   );
 
   const items = results.length ? results : firstPage.page;
-
-  console.log("POSTS TOTALES", totalPosts);
 
   return (
     <section className="flex w-full flex-col gap-6">
@@ -45,15 +47,18 @@ export default function StandardPostList({
           </article>
         ))}
       </div>
-      {/* <div className="mt-8 flex w-full max-w-6xl justify-center">
-                  <PaginationNavigation
-                    currentPage={}
-                    hasNextPage={hasNextPage}
-                    hasPreviousPage={hasPreviousPage}
-                    totalPages={totalPages}
-          />
+      <div className="mt-8 flex w-full max-w-6xl justify-center">
+        <HomePagination
+          currentPage={currentPage}
+          hasNextPage={status === "CanLoadMore"}
+          hasPreviousPage={currentPage > 1}
+          nextPageAction={() => {
+            loadMore(DEFAULT_LATEST_POSTS_LIMIT);
+            setCurrentPage((page) => page + 1);
+          }}
+          totalPages={totalPages}
+        />
       </div>
-      */}
     </section>
   );
 }
